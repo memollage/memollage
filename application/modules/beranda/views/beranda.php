ea2758b3f33607b1a20b6c25efad3e8534e0ae7e
Unknown <jiadaulia@gmail.com>
2018-06-13 03:19:58 +0700

    add tombol notif

--- application/modules/beranda/views/beranda.php
+++ application/modules/beranda/views/beranda.php
@@ -1,18 +1,32 @@
 <html>
+<head>
      <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>asset/theme/css/global.css" />
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+   </head>
+   <body>
 	 <!-- Page Content -->
         <div id="page-wrapper" class="page">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
+                    <div class="col-sm-12">
                         <h1 class="page-header">Memollage</h1>
                     </div>
                     <!-- /.col-lg-12 -->
                 </div>
+                <div class="nav navbar-nav navbar-left">
+                 <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count" style="border-radius:10px;"></span> <span class="glyphicon glyphicon-envelope" style="font-size:18px;"></span></a>
+                  <ul class="dropdown-menu"></ul>
+                 </li>
+               </div>
+
                 <!-- /.row -->
             </div>
             <!-- /.container-fluid -->
         </div>
         <!-- /#page-wrapper -->
-
+      </body>
 </html>
