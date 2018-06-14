2bad2fa8bf36306b4fc444d1d6e993b82616cc51
Unknown <jiadaulia@gmail.com>
2018-06-14 12:28:40 +0700

    huhu

--- application/modules/kelas/ajax/kelas.js
+++ application/modules/kelas/ajax/kelas.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
+     var modal;
      $(".kelas").click(function(){
-          alert(4);
+          modal = document.getElementById('myModal');
+          modal.style.display = "block";
      });
 
+
+     // Get the <span> element that closes the modal
+     var span = document.getElementsByClassName("close")[0];
+
+
+
+     // When the user clicks on <span> (x), close the modal
+     span.onclick = function() {
+         modal.style.display = "none";
+     }
+
+     // When the user clicks anywhere outside of the modal, close it
+     window.onclick = function(event) {
+         if (event.target == modal) {
+             modal.style.display = "none";
+         }
+     }
+
+     $(".btn-menage").click(function() {
+          var x=($(this).data('value'));
+
+          $("#menage").append('<div>'+x+'</div>').addClass("card");
+     });
 });
--- application/modules/kelas/views/kelas.php
+++ application/modules/kelas/views/kelas.php
@@ -6,7 +6,37 @@
      </script>
      <div class="center">
           <div class="row">
-               <div class="col-lg-4 kelas" data-toggle="modal" data-target="#myModal">
+               <div class="col-lg-4 kelas myBtn">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4 kelas myBtn">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4 kelas myBtn">
                     <div class="card">
                           <div class="card-body">
                                <div class="card-content container-in-flex">
@@ -222,23 +252,226 @@
      </div>
 
 
-<!-- Modal -->
-<div id="myModal" class="modal fade" role="dialog">
-  <div class="modal-dialog">
 
-    <!-- Modal content-->
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
+     <!-- The Modal -->
+     <div id="myModal" class="modal">
 
-  </div>
-</div>
+       <!-- Modal content -->
+       <div class="modal-content ">
+         <div class="modal-body body-kelas">
+              <span class="close">&times;</span>
+               <div class="card board-kelas">
+                    <div class="row">
+                         <div class="col-lg-8">
+                              <div class="card board-kelas-in">
+                                   <div class="card-body p-b-0">
+
+                                          <!-- Nav tabs -->
+                                          <ul class="nav nav-tabs customtab" role="tablist">
+                                              <li class="nav-item"> <a class="nav-link active show" data-toggle="tab" href="#home2" role="tab" aria-selected="true"><span class="hidden-sm-up"><i class="ti-home"></i></span> <span class="hidden-xs-down">Data Kelas</span></a> </li>
+                                              <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#profile2" role="tab" aria-selected="false"><span class="hidden-sm-up"><i class="ti-user"></i></span> <span class="hidden-xs-down">Member</span></a> </li>
+                                              <li class="nav-item"> <a class="nav-link" data-toggle="tab" href="#tes" role="tab" aria-selected="false"><span class="hidden-sm-up"><i class="ti-tes"></i></span> <span class="hidden-xs-down">Menage</span></a> </li>
+
+                                          </ul>
+                                          <!-- Tab panes -->
+                                          <div class="tab-content-h">
+                                          <div class="tab-content">
+                                              <div class="tab-pane active show" id="home2" role="tabpanel">
+                                                  <div class="p-20">
+                                                      <h5>Best Clean Tab ever</h5>
+                                                      <h6>you can use it with the small code</h6>
+                                                      <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a.</p>
+                                                  </div>
+                                              </div>
+                                              <div class="tab-pane" id="tes" role="tabpanel">
+                                                   <div class="card" style="margin-left: 10px;margin-right: 10px;">
+                                                        <div class="btn-group btn-group-justified" style="margin-right: auto;margin-left: auto;">
+                                                             <a href="#" class="btn btn-primary btn-menage" data-value="tambahDosen">Add Dosen</a>
+                                                             <a href="#" class="btn btn-primary btn-menage" data-value="tambahKomting">Add komting</a>
+                                                             <a href="#" class="btn btn-primary btn-menage" data-value="tambahAnggota">Tambah Anggota</a>
+                                                             <a href="#" class="btn btn-primary btn-menage" data-value="hapusAnggota">Hapus Anggota</a>
+                                                        </div>
+                                                   </div>
+                                                   <div id=menage class="" style="margin-left: 10px;margin-right: 10px;">
+
+
+                                                   </div>
+                                              </div>
+                                              <div class="tab-pane p-20" id="profile2" role="tabpanel">
+                                                   <div class="card">
+                                                        <div class="recent-comment">
+               									<div class="media">
+               										<div class="media-left">
+               											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+               										</div>
+               										<div class="media-body">
+               											<h4 class="media-heading">john doe</h4>
+               											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+               											<p class="comment-date">October 21, 2018</p>
+               										</div>
+               									</div>
+               									<div class="media">
+               										<div class="media-left">
+               											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+               										</div>
+               										<div class="media-body">
+               											<h4 class="media-heading">john doe</h4>
+               											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+               											<p class="comment-date">October 21, 2018</p>
+               										</div>
+               									</div>
+               									<div class="media">
+               										<div class="media-left">
+               											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+               										</div>
+               										<div class="media-body">
+               											<h4 class="media-heading">john doe</h4>
+               											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+               											<p class="comment-date">October 21, 2018</p>
+               										</div>
+               									</div>
+               									<div class="media">
+               										<div class="media-left">
+               											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+               										</div>
+               										<div class="media-body">
+               											<h4 class="media-heading">john doe</h4>
+               											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+               											<p class="comment-date">October 21, 2018</p>
+               										</div>
+               									</div>
+               									<div class="media">
+               										<div class="media-left">
+               											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+               										</div>
+               										<div class="media-body">
+               											<h4 class="media-heading">john doe</h4>
+               											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+               											<p class="comment-date">October 21, 2018</p>
+               										</div>
+               									</div>
+               									<div class="media">
+               										<div class="media-left">
+               											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+               										</div>
+               										<div class="media-body">
+               											<h4 class="media-heading">john doe</h4>
+               											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+               											<p class="comment-date">October 21, 2018</p>
+               										</div>
+               									</div>
+               									<div class="media">
+               										<div class="media-left">
+               											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+               										</div>
+               										<div class="media-body">
+               											<h4 class="media-heading">john doe</h4>
+               											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+               											<p class="comment-date">October 21, 2018</p>
+               										</div>
+               									</div>
+               									<div class="media">
+               										<div class="media-left">
+               											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+               										</div>
+               										<div class="media-body">
+               											<h4 class="media-heading">john doe</h4>
+               											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+               											<p class="comment-date">October 21, 2018</p>
+               										</div>
+               									</div>
+               								</div>
+                                                   </div>
+                                              </div>
+
+                                          </div>
+                                          </div>
+                                      </div>
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="card board-kelas-in" style="max-height: 460px;">
+                                   <div class="card-title">
+                                        <h4>Message </h4>
+                                   </div>
+                                   <div class="con-scroll" style="overflow: auto;">
+								<div class="recent-comment">
+									<div class="media">
+										<div class="media-left">
+											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+										</div>
+										<div class="media-body">
+											<h4 class="media-heading">john doe</h4>
+											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+											<p class="comment-date">October 21, 2018</p>
+										</div>
+									</div>
+									<div class="media">
+										<div class="media-left">
+											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+										</div>
+										<div class="media-body">
+											<h4 class="media-heading">john doe</h4>
+											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+											<p class="comment-date">October 21, 2018</p>
+										</div>
+									</div>
+									<div class="media">
+										<div class="media-left">
+											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+										</div>
+										<div class="media-body">
+											<h4 class="media-heading">john doe</h4>
+											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+											<p class="comment-date">October 21, 2018</p>
+										</div>
+									</div>
+									<div class="media">
+										<div class="media-left">
+											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+										</div>
+										<div class="media-body">
+											<h4 class="media-heading">john doe</h4>
+											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+											<p class="comment-date">October 21, 2018</p>
+										</div>
+									</div>
+
+									<div class="media">
+										<div class="media-left">
+											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+										</div>
+										<div class="media-body">
+											<h4 class="media-heading">john doe</h4>
+											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+											<p class="comment-date">October 21, 2018</p>
+										</div>
+									</div>
+
+									<div class="media no-border">
+										<div class="media-left">
+											<a href="#"><img alt="..." src="<?php echo base_url();?>asset/theme/vendor/images/avatar/1.jpg" class="media-object"></a>
+										</div>
+										<div class="media-body">
+											<h4 class="media-heading">Mr. Michael</h4>
+											<p>Cras sit amet nibh libero, in gravida nulla. </p>
+											<div class="comment-date">October 21, 2018</div>
+										</div>
+									</div>
+								</div>
+                                   </div>
+                              </div>
+                              <div class="text-right" style="display:flex;background-color:#fcfcfc;">
+
+                                   <textarea name="name" rows="1" cols="1" class="form-control">
+                                   </textarea>
+                                   <button class="btn btn-purple waves-effect waves-light"><i class="fa fa-send m-l-10"></i> </button>
+                              </div>
+                         </div>
+                    </div>
+
+               </div>
+         </div>
+       </div>
+
+     </div>
