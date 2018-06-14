0a5dccd5c502290414f923ff7b19ce28577ccf18
Unknown <jiadaulia@gmail.com>
2018-06-14 10:54:48 +0700

    huhu

--- application/modules/beranda/views/beranda.php
+++ application/modules/beranda/views/beranda.php
@@ -1,23 +1,57 @@
-<html>
+<!-- Page Content -->
 
-     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>asset/theme/css/global.css" />
+     <div class="container-in-flex">
+          <div class="col-lg-8">
+               <div class="card">
+                     <div class="card-body">
+                         <h4 class="card-title">Count Down</h4>
+                          <div class="card-content">
+                               <h1>Buat yang mantap</h1>
+                          </div>
+                     </div>
+               </div>
+          </div>
+          <div class="col-lg-4">
+               <div class="card">
+                    <div class="card-body">
+                         <h4 class="card-title">Todo -> ini akan todo</h4>
+                         <div class="card-content">
+                              <div class="todo-list">
+                                   <div class="tdl-holder">
+                                        <div class="tdl-content">
+                                             <ul>
+                                                  <li>
+                                                       <label>
+                                                            <input type="checkbox"><i class="bg-primary"></i><span>Build an angular app</span>
+                                                            <a href="#" class="ti-close"></a>
+                                                       </label>
+                                                  </li>
+                                                  <li>
+                                                       <label>
+                                                            <input checked="" type="checkbox"><i class="bg-success"></i><span>Creating component page</span>
+                                                            <a href="#" class="ti-close"></a>
+                                                       </label>
+                                                  </li>
+                                                  <li>
+                                                       <label>
+                                                            <input checked="" type="checkbox"><i class="bg-warning"></i><span>Follow back those who follow you</span>
+                                                            <a href="#" class="ti-close"></a>
+                                                       </label>
+                                                  </li>
+                                                  <li>
+                                                       <label>
+                                                            <input checked="" type="checkbox"><i class="bg-danger"></i><span>Design One page theme</span>
+                                                            <a href="#" class="ti-close"></a>
+                                                       </label>
+                                                  </li>
 
-
-	 <!-- Page Content -->
-        <div id="page-wrapper" class="page">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">Memollage</h1>
+                                             </ul>
+                                        </div>
+                                        <input class="tdl-new form-control" placeholder="Type here" type="text">
+                                   </div>
+                              </div>
+                         </div>
                     </div>
-                    <!-- /.col-lg-12 -->
-                </div>
-
-
-                <!-- /.row -->
-            </div>
-            <!-- /.container-fluid -->
-        </div>
-        <!-- /#page-wrapper -->
-
-</html>
+               </div>
+          </div>
+     </div>
---
+++ application/modules/kelas/views/kelas.php
@@ -0,0 +1,244 @@
+     <link href="<?php echo base_url();?>asset/theme/vendor/css/kelas.css" rel="stylesheet">
+     <script type="text/javascript">
+     <?php
+          include APPPATH ."modules/kelas/ajax/kelas.js";
+     ?>
+     </script>
+     <div class="center">
+          <div class="row">
+               <div class="col-lg-4 kelas" data-toggle="modal" data-target="#myModal">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+
+               <div class="col-lg-4 kelas">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+
+               <div class="col-lg-4 kelas">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+               <div class="col-lg-4">
+                    <div class="card">
+                          <div class="card-body">
+                               <div class="card-content container-in-flex">
+                                    <div class="header-kelas">
+                                        <img class="logo" src="<?php echo base_url();?>asset/theme/img/Logo.png" width="60">
+                                   </div>
+                                   <div class="content-data-kelas">
+                                        <p class="nama-kelas text-kelas"> Pemograman Berbasis Web<br>
+                                             <span style="color:red">11.00-12.00 pm</span></p>
+                                   </div>
+                               </div>
+                          </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
