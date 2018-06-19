da02383cb18927f61d3faf71502f7167396f34ef
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-06-20 06:19:43 +0700

    edit controllers sigup

--- application/modules/signUp/controllers/SignUp.php
+++ application/modules/signUp/controllers/SignUp.php
@@ -77,109 +77,150 @@ class SignUp extends CI_Controller {
 	    	else
 	    	{
                if(strcmp($login,"dosen")==0){
-                    $tabel = "dosen";
-     			$where=sprintf("WHERE email='%s' AND password='%s'",$data["email"],$data["password"]);
-     			$result = $this->Model_lib->Cek($tabel,$where);
-                    if($result->num_rows()>0){
-                         $tabel = "dosen";
-          			$where=sprintf("WHERE email='%s' AND password='%s' AND aktivasi=1",$data["email"],$data["password"]);
-          			$result = $this->Model_lib->Cek($tabel,$where);
-                              if($result->num_rows()>0){
-                                   $err="email telah terdaftar";
-                                   $klas="";
-                              }else {
-                                   $err="email telah terdaftar namun belum terverifikasi";
-                                   $klas="";
-                              }
 
-                    }else{
-
-                         $data["id_univ"]="1";
-                         $data["tanggal_daftar"]=date("Y-m-d");
-                         $data["id_akses"]="2";
-                         $data["aktivasi"]="0";
-
-                         $tabel = "dosen";
-
-                         $result = $this->Model_lib->insert($tabel,$data);
-
-                         $tabelA = "aktivasi";
-                         $dataA["email"]=$data["email"];
-                         $dataA["kode"]= hash('sha256',rand());
-
-                         $resultA = $this->Model_lib->insert($tabelA,$dataA);
-
-                         $tabel = "aktivasi";
-          			$where=sprintf("WHERE email='%s' AND kode='%s'",$dataA["email"],$dataA["kode"]);
-          			$result = $this->Model_lib->Cek($tabel,$where);
-                         $id=$result->row();
-
-                         $_SESSION["daftarE"]=$data["email"];
-                         $_SESSION["daftarN"]=$data["nama"];
-                         $_SESSION["table"]="dosen";
-                         $_SESSION["kode"]=$dataA["kode"];
-                         $_SESSION["id"]=$id->id;
-
-                         $err="s";
+                    if($this->isEmailDosen($data["email"])==0){
+                         $err="domain email belum terdaftar dalam list universitas";
                          $klas="";
+                    }
+                    else {
+                         $tabel = "dosen";
+          			$where=sprintf("WHERE email='%s' AND password='%s'",$data["email"],$data["password"]);
+          			$result = $this->Model_lib->Cek($tabel,$where);
+                         if($result->num_rows()>0){
+                              $tabel = "dosen";
+               			$where=sprintf("WHERE email='%s' AND password='%s' AND aktivasi=1",$data["email"],$data["password"]);
+               			$result = $this->Model_lib->Cek($tabel,$where);
+                                   if($result->num_rows()>0){
+                                        $err="email telah terdaftar";
+                                        $klas="";
+                                   }else {
+                                        $err="email telah terdaftar namun belum terverifikasi";
+                                        $klas="";
+                                   }
+
+                         }else{
+                              $tabel="universitas";
+                              $where=sprintf("WHERE email_at='%s'",$this->trimEmail($data["email"]));
+                    		$result = $this->Model_lib->SelectWhere($tabel,$where)->row();
+
+                              $data["id_univ"]=$result->id;
+                              $data["tanggal_daftar"]=date("Y-m-d");
+                              $data["id_akses"]="2";
+                              $data["aktivasi"]="0";
+
+                              $tabel = "dosen";
+
+                              $result = $this->Model_lib->insert($tabel,$data);
+
+                              $tabelA = "aktivasi";
+                              $dataA["email"]=$data["email"];
+                              $dataA["kode"]= hash('sha256',rand());
+
+                              $resultA = $this->Model_lib->insert($tabelA,$dataA);
+
+                              $tabel = "aktivasi";
+               			$where=sprintf("WHERE email='%s' AND kode='%s'",$dataA["email"],$dataA["kode"]);
+               			$result = $this->Model_lib->Cek($tabel,$where);
+                              $id=$result->row();
+
+                              $_SESSION["daftarE"]=$data["email"];
+                              $_SESSION["daftarN"]=$data["nama"];
+                              $_SESSION["table"]="dosen";
+                              $_SESSION["kode"]=$dataA["kode"];
+                              $_SESSION["id"]=$id->id;
+
+                              $err="s";
+                              $klas="";
+                         }
                     }
                }
                else {
-                    $tabel = "mahasiswa";
-     			$where=sprintf("WHERE email='%s' AND password='%s'",$data["email"],$data["password"]);
-                    $result = $this->Model_lib->Cek($tabel,$where);
-
-     			$result = $this->Model_lib->Cek($tabel,$where);
-
-                    if($result->num_rows()>0){
-                         $tabel = "mahasiswa";
-          			$where=sprintf("WHERE email='%s' AND password='%s' AND aktivasi=1",$data["email"],$data["password"]);
-          			$result = $this->Model_lib->Cek($tabel,$where);
-                              if($result->num_rows()>0){
-                                   $err="email telah terdaftar";
-                                   $klas="";
-                              }else {
-                                   $err="email telah terdaftar namun belum terverifikasi";
-                                   $klas="";
-                              }
-
-                    }else{
-
-
-
-                         $data["tanggal_daftar"]=date("Y-m-d");
-                         $data["id_akses"]="4";
-                         $data["aktivasi"]="0";
-
-                         $tabel = "mahasiswa";
-
-                         $result = $this->Model_lib->insert($tabel,$data);
-
-                         $tabelA = "aktivasi";
-                         $dataA["email"]=$data["email"];
-                         $dataA["kode"]= hash('sha256',rand());
-
-                         $resultA = $this->Model_lib->insert($tabelA,$dataA);
-
-                         $tabel = "aktivasi";
-          			$where=sprintf("WHERE email='%s' AND kode='%s'",$dataA["email"],$dataA["kode"]);
-          			$result = $this->Model_lib->Cek($tabel,$where);
-                         $id=$result->row();
-
-                         $_SESSION["daftarE"]=$data["email"];
-                         $_SESSION["daftarN"]=$data["nama"];
-                         $_SESSION["table"]="mahasiswa";
-                         $_SESSION["kode"]=$dataA["kode"];
-                         $_SESSION["id"]=$id->id;
-
-                         $err="s";
+                    if($this->isEmailDosen($data["email"])!=0){
+                         $err="domain email terdaftar dalam list universitas \n anda tidak dapat mendaftar sebagai mahasiswa dengan email ini";
                          $klas="";
-
                     }
+                    else {
+
+                         $tabel = "mahasiswa";
+          			$where=sprintf("WHERE email='%s' AND password='%s'",$data["email"],$data["password"]);
+                         $result = $this->Model_lib->Cek($tabel,$where);
+
+          			$result = $this->Model_lib->Cek($tabel,$where);
+
+                         if($result->num_rows()>0){
+                              $tabel = "mahasiswa";
+               			$where=sprintf("WHERE email='%s' AND password='%s' AND aktivasi=1",$data["email"],$data["password"]);
+               			$result = $this->Model_lib->Cek($tabel,$where);
+                                   if($result->num_rows()>0){
+                                        $err="email telah terdaftar";
+                                        $klas="";
+                                   }else {
+                                        $err="email telah terdaftar namun belum terverifikasi";
+                                        $klas="";
+                                   }
+
+                         }else{
+
+                              $data["tanggal_daftar"]=date("Y-m-d");
+                              $data["id_akses"]="4";
+                              $data["aktivasi"]="0";
+
+                              $tabel = "mahasiswa";
+
+                              $result = $this->Model_lib->insert($tabel,$data);
+
+                              $tabelA = "aktivasi";
+                              $dataA["email"]=$data["email"];
+                              $dataA["kode"]= hash('sha256',rand());
+
+                              $resultA = $this->Model_lib->insert($tabelA,$dataA);
+
+                              $tabel = "aktivasi";
+               			$where=sprintf("WHERE email='%s' AND kode='%s'",$dataA["email"],$dataA["kode"]);
+               			$result = $this->Model_lib->Cek($tabel,$where);
+                              $id=$result->row();
+
+                              $_SESSION["daftarE"]=$data["email"];
+                              $_SESSION["daftarN"]=$data["nama"];
+                              $_SESSION["table"]="mahasiswa";
+                              $_SESSION["kode"]=$dataA["kode"];
+                              $_SESSION["id"]=$id->id;
+
+                              $err="s";
+                              $klas="";
+
+                         }
+                    }
+
                }
 
 	     }
 	    		$arr = array('err'=>$err,'klas'=>$klas);
 	    echo json_encode($arr);
      }
+     public function isEmailDosen($value)
+	{
+		$i=0;
+		for (; $i<strlen($value); $i++) {
+			if($value[$i]=='@'){
+				break;
+			}
+		}
+
+		$data=substr($value,$i+1);
+          $tabel="universitas";
+          $where=sprintf("WHERE email_at='%s'",$data);
+		$result = $this->Model_lib->SelectWhere($tabel,$where)->num_rows();
+          return $result;
+	}
+     public function trimEmail($value)
+	{
+		$i=0;
+		for (; $i<strlen($value); $i++) {
+			if($value[$i]=='@'){
+				break;
+			}
+		}
+		return substr($value,$i+1);
+	}
 }
