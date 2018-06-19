91fce01d10b91d9538e1869156891b338f5f8730
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-06-20 06:15:52 +0700

    edit view notif

--- application/modules/notif/views/notif.php
+++ application/modules/notif/views/notif.php
@@ -1,9 +1,11 @@
-<ul>
-    <li>
-        <div class="drop-title">Notifications</div>
-    </li>
-    <li>
-        <div class="message-center">
+
+<script type="text/javascript">
+<?php
+     //include APPPATH ."modules/notif/ajax/notif.js";
+     //include APPPATH ."modules/kelas/ajax/kelasElement.js";
+?>
+</script>
+
             <!-- Message -->
             <a href="#">
                <div class="btn btn-danger btn-circle m-r-10"><i class="fa fa-link"></i></div>
@@ -32,9 +34,3 @@
                     <h5>This is another title</h5> <span class="mail-desc">Just see the my admin!</span> <span class="time">9:02 AM</span>
                </div>
             </a>
-        </div>
-    </li>
-    <li>
-        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
-    </li>
-</ul>
