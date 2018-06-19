f26e92c88578998c83cb53f0f8faa88c52311fb0
Muammar Zikri Aksana <muammar.clasic@gmail.com>
2018-06-20 06:20:34 +0700

    tambah fungsi klik foto profile di header js

--- asset/theme/vendor/js/header.js
+++ asset/theme/vendor/js/header.js
@@ -2,17 +2,23 @@ var con=0;
 window.onload=loadF();
 
 $(document).ready(function(){
+   loadNotif();
    $(".menu").click(function(){
           var z = $(this).data("value");
           var url = BASE_URL+z;
           $.post( url, { x : 4 }).done(function(data) {
+               if(z=="kelas/open"){
+                    FLAG_INIT_CHAT=0;
+               }
                $('#page-content').html(data);
           });
    });
    $(".nav-link").click(function(){
+
           var z = $(this).data("value");
           var url = BASE_URL+z+"/open";
           $.post( url, { x : 4 }).done(function(data) {
+               FLAG_INIT_CHAT=1;
                $('#'+z).html(data);
           });
    });
@@ -22,6 +28,7 @@ function loadF() {
      var z = "beranda"+"/open";
      var url = BASE_URL+z;
      $.post( url, { x : 4 }).done(function(data) {
+          FLAG_INIT_CHAT=1;
           $('#page-content').html(data);
      });
 }
@@ -40,4 +47,23 @@ function resizeNav() {
           $(".modal-content").width('83.5%');
 
      }
+}
+
+
+function profileUserML(x) {
+
+     FLAG_INIT_CHAT=1;
+     var email=$(x).attr("data-value");
+     var url = BASE_URL+"akun/otherAkun/"+email;
+     //alert(url);
+     $.post(url).done(function(data) {
+          $('#page-content').html(data);
+     });
+}
+
+function logout() {
+     var url = BASE_URL+"signIn/logout";
+     $.post(url).done(function(data) {
+          window.open(BASE_URL+'signIn',"_self");
+     });
 }
