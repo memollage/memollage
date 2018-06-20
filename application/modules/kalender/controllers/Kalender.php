a6785056ad08f67a91146b9daaceb30a3ead1328
Gilang Gemilang <gemilanggilangvi@gmail.com>
2018-06-20 23:09:06 +0700

    debut

---
+++ application/modules/kalender/controllers/Kalender.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class kalender extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	function __construct() {
+		parent::__construct();
+		$this->_public_view= $this->config->item('public_view');
+	}
+
+	public function index()
+	{
+		$data=array('page_content' => 'kalender');
+		$this->load->view($this->_public_view,$data);
+	}
+	/*public function index()
+	{
+		$this->load->view('berand	a');
+	}*/
+	public function open()
+	{
+		$this->load->view('kalender');
+	}
+}
---
+++ application/modules/kalender/views/kalender.php
@@ -0,0 +1,82 @@
+<link href="<?php echo base_url();?>asset/theme/css/jadwal.css" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+  <!--
+  CSS
+  ============================================= -->
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/linearicons.css">=
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/font-awesome.min.css">
+  <link href="<?php echo base_url();?>asset/theme/css/jadwal/availability-calendar.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/magnific-popup.css">
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/bootstrap.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/main.css">
+
+<section class="calender-area relative section-gap">
+  <div class="overlay overlay-bg"></div>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-8 pb-80 header-text">
+        <h1>Event Calendar</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> labore  et dolore magna aliqua.
+        </p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 calender-wrap" id="calendar"></div>
+      <div class="col-lg-12 event-dates">
+        <div class="single-event d-flex flex-row">
+          <p class="col">
+            Dec 01
+          </p>
+          <p class="col-8">
+            World AIDS Day
+          </p>
+          <p class="col text-right">
+            <span class="lnr lnr-highlight"></span>
+            <span class="lnr lnr-trash"></span>
+          </p>
+        </div>
+        <div class="single-event d-flex flex-row">
+          <p class="col">
+            Dec 16
+          </p>
+          <p class="col-8">
+            Victory Day of people republic of Bangladesh
+          </p>
+          <p class="col text-right">
+            <span class="lnr lnr-highlight"></span>
+            <span class="lnr lnr-trash"></span>
+          </p>
+        </div>
+        <div class="single-event d-flex flex-row">
+          <p class="col">
+            Dec 25
+          </p>
+          <p class="col-8">
+            Chrismas Day Arrangement
+          </p>
+          <p class="col text-right">
+            <span class="lnr lnr-highlight"></span>
+            <span class="lnr lnr-trash"></span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End calender Area -->
+<!-- End footer Area -->
+
+<script src="<?php echo base_url();?>asset/theme/vendor/jquery/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="<?php echo base_url();?>asset/theme/vendor/bootstrap/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/jquery.ajaxchimp.min.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/availability-calendar.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/jquery.sticky.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/parallax.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/countdown.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/main.js"></script>
