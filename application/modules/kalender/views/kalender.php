8cb4e1e552c095ea7debc8946c41b3b195df5e8b
Gilang Gemilang <gemilanggilangvi@gmail.com>
2018-06-21 04:07:57 +0700

    s

--- application/modules/kalender/views/kalender.php
+++ application/modules/kalender/views/kalender.php
@@ -13,12 +13,32 @@
     </div>
 
     <div class="row">
-      <div class="col-lg-12 calender-wrap" id="calendar"></div>
+      <a class="button" href="#popup">
+        <div class="col-lg-12 calender-wrap" id="calendar"></div>
+      </a>
+      <div class="popup" id="popup">
+        <div class="popup-inner">
+          <div class="popup__photo">
+            <img src="https://images.unsplash.com/photo-1515224526905-51c7d77c7bb8?ixlib=rb-0.3.5&s=9980646201037d28700d826b1bd096c4&auto=format&fit=crop&w=700&q=80" alt="">
+          </div>
+          <div class="popup__text">
+            <h1>Lorem ipsum dolor sit amet</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ex velit, viverra non vulputate vitae, blandit vitae nisl. Nullam fermentum orci et erat viverra bibendum. Aliquam sed varius nibh, vitae mattis purus. Mauris elementum sapien non ullamcorper vulputate. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed eget felis sit amet eros viverra pulvinar.</p>
+          </div>
+          <a class="popup__close" href="#">X</a>
+        </div>
+      </div>
 
+  <a class="buttontambah" href="#popup">+</a>
+  <div class="popup" id="popup">
+    <div class="popup-inner">
+      <a class="popup__close" href="#">X</a>
     </div>
   </div>
-</section>
+    </div>
 
+  </div>
+</section>
 <div class="col-lg-12 event-dates card">
   <div class="single-event d-flex flex-row">
     <p class="col">
---
+++ application/modules/roster/views/roster.php
@@ -0,0 +1,77 @@
+<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+  <!--
+  CSS
+  ============================================= -->
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/linearicons.css">=
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link href="<?php echo base_url();?>asset/theme/css/jadwal/availability-calendar.css" rel="stylesheet">
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/magnific-popup.css">
+  <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/bootstrap.css">
+  <link rel="stylesheet" href="<?php echo base_url();?>asset/theme/css/jadwal/main.css">
+
+<section class="calender-area relative section-gap">
+  <div class="overlay overlay-bg"></div>
+  <div class="container">
+    <div class="row d-flex justify-content-center">
+      <div class="col-md-8 pb-80 header-text">
+        <h1>Calendar</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-12 calender-wrap" id="calendar"></div>
+      <div class="col-lg-12 event-dates">
+        <div class="single-event d-flex flex-row">
+          <p class="col">
+            Dec 01
+          </p>
+          <p class="col-8">
+            World AIDS Day
+          </p>
+          <p class="col text-right">
+            <span class="lnr lnr-highlight"></span>
+            <span class="lnr lnr-trash"></span>
+          </p>
+        </div>
+        <div class="single-event d-flex flex-row">
+          <p class="col">
+            Dec 16
+          </p>
+          <p class="col-8">
+            Victory Day of people republic of Bangladesh
+          </p>
+          <p class="col text-right">
+            <span class="lnr lnr-highlight"></span>
+            <span class="lnr lnr-trash"></span>
+          </p>
+        </div>
+        <div class="single-event d-flex flex-row">
+          <p class="col">
+            Dec 25
+          </p>
+          <p class="col-8">
+            Chrismas Day Arrangement
+          </p>
+          <p class="col text-right">
+            <span class="lnr lnr-highlight"></span>
+            <span class="lnr lnr-trash"></span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- End calender Area -->
+<!-- End footer Area -->
+
+<script src="<?php echo base_url();?>asset/theme/vendor/jquery/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="<?php echo base_url();?>asset/theme/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/jquery.ajaxchimp.min.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/availability-calendar.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/jquery.sticky.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/parallax.min.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/countdown.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo base_url();?>asset/theme/js/jadwal/main.js"></script>
